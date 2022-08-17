222bd8a87bf544d173cd579837c950ae362da04b
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-17 16:57:26 +0300

    Edited the file and served the errors close #15

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const quizRouter = require('./controllers');
+const { error404, serverError } = require('./errors');
 
 router.get('/quiz', quizRouter);
+router.use('*', error404);
+router.use(serverError);
 
 module.exports = router;
