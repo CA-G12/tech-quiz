98a90b353cfcc5b757e036f03e74587a88758bfa
Mustafa Salem <mustafaissasalem@gmail.com>
2022-08-18 09:44:12 +0300

    Fixed returning the data process

--- src/controllers/questions.js
+++ src/controllers/questions.js
@@ -1,10 +1,13 @@
 const path = require('path');
-const fs = require('fs');
-const sentQuestions = (req, res , next) => {
- 
-      res.status(200).sendFile(path.join(__dirname, '..', 'data.json'));
-    
 
+const fs = require('fs');
+
+const sentQuestions = (req, res, next) => {
+  const filePath = path.join(__dirname, '..', 'data.json');
+  fs.readFile(filePath, (err, data) => {
+    if (err) next(err);
+    res.status(200).json(JSON.parse(data.toString()));
+  });
 };
 
 module.exports = sentQuestions;
